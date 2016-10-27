fd3eaf9f587f821c24ccb36ef78a612ac629aa19
home <home@LAPTOP-CLEMU8AQ>
2016-10-27 22:49:07 +0530

    commited

---
+++ hbArtifatId/src/main/java/hbGroupId/hbArtifatId/App.java
@@ -0,0 +1,13 @@
+package hbGroupId.hbArtifatId;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
